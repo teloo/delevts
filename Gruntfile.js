d40cf297af241d9db4c6533fd4a82fc02f528b77
Teloo <teru1721@gmail.com>
2014-09-13 14:00:55 +0900

    Enable to execute grunt and jshint

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  var jsFiles = [
+    'bin/delevts',
+    'lib/**/*.js'
+  ];
+
+  grunt.initConfig({
+    watch: {
+      scripts: {
+        files: jsFiles,
+        tasks: ['default']
+      }
+    },
+    jshint: {
+      all: jsFiles,
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('hint', ['jshint']);
+  grunt.registerTask('default', ['hint']);
+};
